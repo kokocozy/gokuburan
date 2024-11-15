ac72ec5c416531d4283d5b3bb9a18f825d1f0af1
Muhammad Farkhan Syafii <zheninjas75@yahoo.co.id>
2024-11-16 06:48:27 +0700

    fix: test

--- tests/Feature/GraveTest.php
+++ tests/Feature/GraveTest.php
@@ -9,18 +9,18 @@ use App\Models\Grave;
 use App\Models\User;
 use function Pest\Livewire\livewire;
 
-// beforeEach(function () {
-//     $this->actingAs(User::where('role', 'admin')->first());
-// });
-
-// it('can list graves', function () {
-//     $graves = Grave::limit(10)->get();
-//     livewire(ListGraves::class)
-//         ->assertCanSeeTableRecords($graves);
-// });
-
-it('can render page', function () {
-    $this->withoutMiddleware();
-    $this->get(GraveResource::getUrl('create'))->assertSuccessful();
+beforeEach(function () {
+    $this->actingAs(User::where('role', 'admin')->first());
 });
+
+it('can list graves', function () {
+    $graves = Grave::limit(10)->get();
+    livewire(ListGraves::class)
+        ->assertCanSeeTableRecords($graves);
+});
+
+// it('can render page', function () {
+//     $this->withoutMiddleware();
+//     $this->get(GraveResource::getUrl('create'))->assertSuccessful();
+// });
 
